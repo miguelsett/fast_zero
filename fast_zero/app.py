e569832f62d568fa93d6475e28b127f9219e7244
miguelsett <miguelsettimio@gmail.com>
2025-05-07 01:00:47 -0300

    Testando o Endpoint GET /users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -10,6 +10,8 @@ from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
 from fast_zero.database import get_session
-from fast_zero.models import table_registry
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture
@@ -55,3 +55,13 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
