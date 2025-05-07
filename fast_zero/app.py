570feacf6de965eaac063e1d123d6af2843974d0
miguelsett <miguelsettimio@gmail.com>
2025-05-07 00:29:33 -0300

    Implementando o teste da rota de DELETE

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -30,19 +30,13 @@ def read_users():
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1: 
-
-
+    if user_id > len(database) or user_id < 1:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        ) 
-
-
+        )
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id 
-
-
+    database[user_id - 1] = user_with_id
 
     return user_with_id
 
