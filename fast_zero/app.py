9a3cfaa4989259cb3751f22ae2b61e2ef675f436
miguelsett <miguelsettimio@gmail.com>
2025-05-07 00:19:07 -0300

    Iniciando a implementação da rota POST

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -10,3 +10,9 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    ...
