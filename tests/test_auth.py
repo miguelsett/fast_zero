e8c45fddbcf36df0959db6dec0e4bd466082f30d
miguelsett <miguelsettimio@gmail.com>
2025-05-08 22:49:36 -0300

    Testando o usuário não existente e senha incorreta

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -36,3 +36,21 @@ def test_token_expired_after_time(client, user):
         )
         assert response.status_code == HTTPStatus.UNAUTHORIZED
         assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_inexistent_user(client):
+    response = client.post(
+        '/auth/token',
+        data={'username': 'no_user@no_domain.com', 'password': 'testtest'},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_token_wrong_password(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': user.email, 'password': 'wrong_password'},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
