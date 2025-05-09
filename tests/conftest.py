4b97aba1b7097ecde38e6f545329ee0aee0e655d
miguelsett <miguelsettimio@gmail.com>
2025-05-08 22:20:21 -0300

    Endpoints não bloqueantes

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,8 +61,8 @@ def mock_db_time():
     return _mock_db_time
 
 
-@pytest.fixture
-def user(session):
+@pytest_asyncio.fixture
+async def user(session):
     password = 'testtest'
     user = User(
         username='Teste',
@@ -70,8 +70,8 @@ def user(session):
         password=get_password_hash(password),
     )
     session.add(user)
-    session.commit()
-    session.refresh(user)
+    await session.commit()
+    await session.refresh(user)
 
     user.clean_password = password
 
